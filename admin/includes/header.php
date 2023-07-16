feb01663bd5f71abe18b42e07d34893a019c6548
bkusal <136067453+bkusal@users.noreply.github.com>
2023-07-16 12:32:10 +0545

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="header">
+          <!-- navbar -->
+     <nav class="navbar navbar-expand-lg navbar-light ">
+     <a class="navbar-brand" href="indexa.php">CRM System</a>
+  <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="logout.php" class="nav-link">Logout</a>
+        </li>
+      
+    </ul>
+  </div>
+</nav>
+    </div>
+</body>
+</html>
